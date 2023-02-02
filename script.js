8560706aad0996af98fad56edceb9809359896db
Digital Knight <digitalknight@Digitals-MacBook-Pro.local>
2023-02-02 15:46:08 +0100

    Updated tabs list and js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+  const tabs = document.querySelectorAll(".tab-name");
+  const tabContentContainers = document.querySelectorAll(".tab-content");
 
+  tabs.forEach(function (tab) {
+    tab.addEventListener("click", function (event) {
+      tabContentContainers.forEach(function (container) {
+        container.classList.remove("active");
+      });
+
+      const contentElement = document.querySelector(
+        `[data-content="${this.dataset.tab}"]`
+      );
+
+      contentElement.classList.add("active");
+    });
+  });
+});
